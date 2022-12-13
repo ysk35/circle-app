e225efd9415575ff2b3d6c3fcebe452e4be4c2de
ysk35 <ysk35.777@icloud.com>
2022-12-14 02:22:08 +0900

    add attendance table

---
+++ create_attendance_table.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date
+from setting import Base
+from setting import ENGINE
+
+
+class Attendance(Base):
+  __tablename__ = "attendance"
+  id = Column('id', Integer, primary_key = True)
+  user_id = Column('usser_id', Integer)
+  name = Column('name', String(200))
+  date = Column('date', Date)
+
+if __name__ == "__main__":
+  Base.metadata.create_all(bind=ENGINE)
